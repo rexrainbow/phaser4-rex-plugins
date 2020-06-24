dc4494571f709aa47648d4ba72fae23d17c7c3b0
Rex <binhua.tein@gmail.com>
2020-06-24 17:06:34 +0800

    Port some code

---
+++ src/board/board/ILogicBoard.ts
@@ -0,0 +1,25 @@
+import { IGrid } from '../grid/IGrid';
+
+export interface IConfig {
+    isBoard?: boolean;
+    grid?: IGrid;
+    wrap?: boolean;
+    inifinity?: boolean;
+    width?: number;
+    height?: number;
+
+}
+
+export interface ILogicBoard {
+    isBoard: boolean;
+    grid: IGrid;
+    wrapMode: boolean;
+    infinityMode: boolean;
+    width: number | undefined;
+    height: number | undefined;
+
+    destroy(): void;
+    setWrapMode(mode?: boolean): this;
+    setInfinityMode(mode?: boolean): this;
+    setBoardSize(width?: number, height?: number): this;
+}
---
+++ src/board/board/LogicBoard.ts
@@ -0,0 +1,54 @@
+import { ILogicBoard, IConfig } from './ILogicBoard';
+import { IGrid } from '../grid/IGrid';
+
+export class LogicBoard implements ILogicBoard {
+    isBoard: boolean;
+    grid: IGrid;
+    wrapMode: boolean;
+    infinityMode: boolean;
+    width: number | undefined;
+    height: number | undefined;
+
+    constructor({
+        isBoard = true,
+        grid = undefined,
+        wrap = false,
+        inifinity = false,
+        width = 0,
+        height = 0
+    }: IConfig = {}) {
+
+        this.isBoard = isBoard;
+        this.setGrid(grid);
+        this.setWrapMode(wrap);
+        this.setInfinityMode(inifinity);
+        this.setBoardSize(width, height);
+    }
+
+    destroy() {
+        
+    }
+
+    setGrid(grid?: IGrid): this {
+
+        this.grid = grid;
+        return this;
+    }
+
+    setWrapMode(mode: boolean = true): this {
+
+        this.wrapMode = mode;
+        return this;
+    }
+
+    setInfinityMode(mode: boolean = true): this {
+
+        this.infinityMode = mode;
+        return this;
+    }
+
+    setBoardSize(width: number = 0, height: number = 0): this {
+
+        return this;
+    }
+}
