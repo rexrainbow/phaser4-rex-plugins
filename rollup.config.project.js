cb945ff7ed2d3477d3e038f768c28f78fe0065ec
Rex <binhua.tein@gmail.com>
2020-07-02 13:02:27 +0800

    Add source code link

--- rollup.config.project.js
+++ rollup.config.project.js
@@ -13,6 +13,9 @@ let path = inputMain.split('/');
 const inputFileName = path.pop().split('.')[0];
 const inputFolder = path.pop();
 const exportFolder = (production) ? `${inputFolder}-${inputFileName}` : '_preview';
+const sourceCodeLink = (production) ?
+    `<p><a href='https://github.com/rexrainbow/phaser4-rex-plugins/blob/master/${inputMain}'>Source code</a></p>` :
+    '';
 
 const capitalize = (s) => {
     return s.charAt(0).toUpperCase() + s.slice(1)
@@ -49,7 +52,8 @@ export default {
             template: templatePath,
             target: `public/${exportFolder}/index.html`,
             replaceVars: {
-                __TITLE__: `${capitalize(inputFolder)}-${capitalize(inputFileName)}`
+                __TITLE__: `${capitalize(inputFolder)}-${capitalize(inputFileName)}`,
+                __SOURCE_CODE__: sourceCodeLink
             }
         })
     ],
