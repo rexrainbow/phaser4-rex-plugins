183f28f516741628b08a443c4364b88ea6f9fd32
Rex <binhua.tein@gmail.com>
2020-07-08 23:46:41 +0800

    Generate index.html after folder created

--- esbuild.build.js
+++ esbuild.build.js
@@ -6,27 +6,7 @@ const inputFileName = path.pop().split('.')[0];
 const inputFolder = path.pop();
 const exportFolder = (production) ? `${inputFolder}-${inputFileName}` : '_preview';
 
-// Generate index.html
-(function () {
-    const templatePath = process.env.template || './examples/preview-template.html'
-    const capitalize = (s) => { return s.charAt(0).toUpperCase() + s.slice(1) }
-    const title = `${capitalize(inputFolder)}-${capitalize(inputFileName)}`
-    const sourceCodeLink = (production) ?
-        `<p><a href='https://github.com/rexrainbow/phaser4-rex-plugins/blob/master/${inputMain}'>Source code</a></p>` :
-        '';
-
-    const fs = require('fs');
-    const Mustache = require('mustache');
-    fs.readFile(templatePath, function (err, data) {
-        data = Mustache.render(data.toString(), {
-            TITLE: title,
-            SOURCE_CODE: sourceCodeLink
-        })
-        fs.writeFile(`./public/${exportFolder}/index.html`, data, () => { });
-    })
-})();
-// Generate index.html
-
+// Generate boundle.js
 const { build } = require('esbuild');
 build({
     entryPoints: [inputMain],
@@ -34,4 +14,24 @@ build({
     minify: production,
     bundle: true,
     sourcemap: true,
-}).catch(() => process.exit(1))
+})
+    .then(function () {
+        // Generate index.html
+        const templatePath = process.env.template || './examples/preview-template.html'
+        const capitalize = (s) => { return s.charAt(0).toUpperCase() + s.slice(1) }
+        const title = `${capitalize(inputFolder)}-${capitalize(inputFileName)}`
+        const sourceCodeLink = (production) ?
+            `<p><a href='https://github.com/rexrainbow/phaser4-rex-plugins/blob/master/${inputMain}'>Source code</a></p>` :
+            '';
+
+        const fs = require('fs');
+        const Mustache = require('mustache');
+        fs.readFile(templatePath, function (err, data) {
+            data = Mustache.render(data.toString(), {
+                TITLE: title,
+                SOURCE_CODE: sourceCodeLink
+            })
+            fs.writeFile(`./public/${exportFolder}/index.html`, data, () => { });
+        })
+    })
+    .catch(() => process.exit(1))
