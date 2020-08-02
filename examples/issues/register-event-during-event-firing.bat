d3de39d579c3378f956194ef2ea35aeb56da7b54
Rex <binhua.tein@gmail.com>
2020-08-02 08:50:41 +0800

    Add issue sample files

---
+++ examples/issues/register-event-during-event-firing.bat
@@ -0,0 +1,6 @@
+@echo off
+set preview=1
+set main=./examples/issues/register-event-during-event-firing.ts
+cd ..
+cd ..
+npm run build-project
---
+++ examples/issues/register-event-during-event-firing.ts
@@ -0,0 +1,31 @@
+import { BackgroundColor, Parent, Scenes, Size, WebGLRenderer } from '@phaserjs/phaser/config';
+import { Game } from '@phaserjs/phaser/Game';
+import { Scene } from '@phaserjs/phaser/scenes/Scene';
+import { Keyboard } from '@phaserjs/phaser/input';
+import { Once } from '@phaserjs/phaser/events'
+
+class Demo extends Scene {
+    constructor() {
+        super();
+
+        let i = 1000;
+        const keyboard = new Keyboard.Keyboard();
+        const onKeyDown = function (event) {
+            if (i < 0) {
+                return;
+            }
+            i--; // To prevent endless event firing
+            console.log(`${event.key.toUpperCase()} key down`)
+            Once(keyboard, 'keydown', onKeyDown);
+        }
+        Once(keyboard, 'keydown', onKeyDown);
+    }
+}
+
+new Game(
+    WebGLRenderer(),
+    Size(800, 600),
+    Parent('game'),
+    BackgroundColor(0x2d2d2d),
+    Scenes(Demo)
+);
