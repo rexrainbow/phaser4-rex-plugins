90c642f9660a374d1022c2fe6157bd24abaef2ab
Rex <binhua.tein@gmail.com>
2020-08-27 10:41:07 +0800

    Test webpack builder but failed

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,24 +1,52 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 const inputMain = process.env.main;  // Required
-const outputFolder = process.env.outputfolder || 'public';
+const production = process.env.preview === '0';
+const preview = !production;
+
+let pathes = inputMain.split('/');
+const inputFileName = pathes.pop().split('.')[0];
+const inputFolder = pathes.pop();
+
+const rootPath = path.resolve(__dirname, '..');
+const exportFolder = (production) ? `${inputFolder}-${inputFileName}` : '_preview';
+const exportFolderPath = `${rootPath}/public/${exportFolder}`;
+const htmlTemplate = process.env.htmltemplate || `${rootPath}/examples/preview-template.html`;
 
 module.exports = {
-    mode: 'development',
+    mode: ((preview) ? 'development' : 'production'),
     entry: inputMain,
     devtool: 'cheap-source-map',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, outputFolder),
+        path: exportFolderPath,
     },
-    watch: true,
+    watch: preview,
     plugins: [
-        new BrowserSyncPlugin({
+        new HtmlWebpackPlugin({
+            filename: `${exportFolderPath}/index.html`,
+            template: htmlTemplate,
+            chunks: ['vendor', 'app'],
+            chunksSortMode: 'manual',
+            minify: {
+                removeAttributeQuotes: false,
+                collapseWhitespace: false,
+                html5: false,
+                minifyCSS: false,
+                minifyJS: false,
+                minifyURLs: false,
+                removeComments: false,
+                removeEmptyAttributes: false
+            },
+            hash: false
+        }),
+        preview && new BrowserSyncPlugin({
             host: process.env.IP || 'localhost',
             port: process.env.PORT || 3000,
             server: {
-                baseDir: outputFolder,
+                baseDir: exportFolderPath,
             },
         })
     ],
@@ -26,9 +54,14 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                use: 'ts-loader',
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: `${rootPath}/tsconfig.build.json`
+                    }
+                }],
                 exclude: /node_modules/,
-            },
+            }
         ]
     },
     node: {
