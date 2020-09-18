9bfb923026c320042df71a1ad93d40687c62b1b9
Rex <binhua.tein@gmail.com>
2020-09-18 09:35:11 +0800

    Add index.ts in gameobject folder

---
+++ src/gameobjects/index.ts
@@ -0,0 +1,16 @@
+import { BaseCanvas, Canvas } from './canvas';
+import { MaskedImage } from './maskedimage';
+import { RoundRectangleCanvas } from './roundrectanglecanvas';
+import { BaseSizer, Sizer } from './sizer';
+import { BBCodeText, TagText } from './text';
+
+export {
+    BBCodeText,
+    BaseCanvas,
+    BaseSizer,
+    Canvas,
+    MaskedImage,
+    RoundRectangleCanvas,
+    Sizer,
+    TagText
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { AwayTime } from './time/awaytime';
-import { BBCodeText, TagText } from './gameobjects/text';
-import * as Board from './board'
-import { Canvas } from './gameobjects/canvas';
+import {
+    BBCodeText,
+    Canvas,
+    MaskedImage,
+    RoundRectangleCanvas,
+    Sizer,
+    TagText
+} from './gameobjects';
+import * as Board from './board';
 import * as CanvasTexture from './texture/canvastexture';
 import { Clock } from './time/clock';
 import { CSVToArray } from './data/csvtoarray';
@@ -12,11 +18,8 @@ import { Gashapon } from './math/gashapon';
 import { LifeTime } from './time/lifetime';
 import { LoopInTicks } from './logic/loopinticks';
 import { LZString } from './string/lzstring';
-import { MaskedImage } from './gameobjects/maskedimage';
 import * as Quest from './logic/quest';
-import { RoundRectangleCanvas } from './gameobjects/roundrectanglecanvas';
 import { RunCommands } from './logic/runcommands';
-import { Sizer } from './gameobjects/sizer'
 import { TwoDTable } from './data/twodtable';
 import { XOR } from './string/xor';
 
