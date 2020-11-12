5818cfab473eaede8eb8a6ea9846a6b6b81b3f0f
Rex <binhua.tein@gmail.com>
2020-11-12 17:17:28 +0800

    Refactory
    Code from phaser3

--- src/utils/object/DeepClone.ts
+++ src/utils/object/DeepClone.ts
@@ -1,6 +1,21 @@
 export function DeepClone<T>(
-    obj: T,
+    inObject: T,
 ): T {
 
-    return JSON.parse(JSON.stringify(obj));
+    if ((typeof inObject !== 'object') || (inObject === null)) {
+        //  inObject is not an object
+        return inObject;
+    }
+
+    //  Create an array or object to hold the values
+    const outObject = (Array.isArray(inObject) ? [] : {}) as T;
+
+    for (const key in inObject) {
+        const value = inObject[key];
+
+        //  Recursively (deep) copy for nested objects, including arrays
+        outObject[key] = DeepClone(value);
+    }
+
+    return outObject;
 }
