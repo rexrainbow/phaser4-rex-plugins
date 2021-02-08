ea2914c06e75664cf79d27f77ddb0994497f18c8
Rex <binhua.tein@gmail.com>
2021-02-08 11:05:41 +0800

    Center iframe

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Notes/plugins of Phaser4 engine
 
-- [Demos](https://rexrainbow.github.io/phaser4-rex-plugins/public)
+- [Demos](https://rexrainbow.github.io/phaser4-rex-plugins/public/home/)
 - [Notes](https://rexrainbow.github.io/phaser4-rex-plugins/doc/build/html)
--- examples/home/src/pages/Example.vue
+++ examples/home/src/pages/Example.vue
@@ -1,33 +1,34 @@
 <template>
-  <div>
-    <iframe
-      :src="iFrameSrc"
-      :width="800"
-      :height="600"
-      frameborder="0"
-      scrolling="no"
-      seamless="seamless"
-    ></iframe>
-  </div>
+    <q-page class="flex flex-center">
+        <iframe
+            :src="iFrameSrc"
+            :width="800"
+            :height="600"
+            frameborder="0"
+            scrolling="no"
+            seamless="seamless"
+        ></iframe>
+    </q-page>
 </template>
 
 <script>
-const IframeRootURL = "https://rexrainbow.github.io/phaser4-rex-plugins/public/";
+const IframeRootURL =
+    "https://rexrainbow.github.io/phaser4-rex-plugins/public/";
 
 export default {
-  name: "Example",
-  data() {
-    return {
-      gameIFrameSrc: ""
-    };
-  },
-  computed: {
-    id() {
-      return this.$route.params.id;
+    name: "Example",
+    data() {
+        return {
+            gameIFrameSrc: "",
+        };
     },
-    iFrameSrc() {
-      return `${IframeRootURL}${this.id}/`;
-    }
-  }
+    computed: {
+        id() {
+            return this.$route.params.id;
+        },
+        iFrameSrc() {
+            return `${IframeRootURL}${this.id}/`;
+        },
+    },
 };
 </script>
