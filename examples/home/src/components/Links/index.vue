efa5f6e6a8be5136ef1c2f9918e99652c2813f70
Rex <binhua.tein@gmail.com>
2021-02-08 10:53:13 +0800

    Replace expand items by tree

--- examples/home/src/components/Links/index.vue
+++ examples/home/src/components/Links/index.vue
@@ -1,21 +1,58 @@
 <template>
-  <q-list>
-    <ExpandLinks
-      v-for="(child, childName) in links"
-      :key="childName"
-      prefix=""
-      :parent="childName"
-      :children="child"
-    />
-  </q-list>
+    <q-tree
+        :nodes="dataToNode(links)"
+        node-key="nodeKey"
+        default-expand-all
+        no-connectors
+    >
+        <template v-slot:header-link="prop">
+            <q-item :to="prop.node.nodeKey" exact dense>
+                <q-item-section>
+                    {{ prop.node.label }}
+                </q-item-section>
+            </q-item>
+        </template>
+    </q-tree>
 </template>
 
 <script>
-import ExpandLinks from "./ExpandLinks";
+var DataTONode = function (data, parent) {
+    var nodes = [];
+    if (!Array.isArray(data)) {
+        for (var name in data) {
+            var node = {
+                label: name,
+                header: "category",
+                nodeKey: GetNodeKey(name, parent),
+            };
+            node.children = DataTONode(data[name], node);
+            nodes.push(node);
+        }
+    } else {
+        data.forEach(function (name) {
+            nodes.push({
+                label: name,
+                header: "link",
+                nodeKey: GetNodeKey(name, parent),
+            });
+        });
+    }
+
+    return nodes;
+};
+
+var GetNodeKey = function (name, parent) {
+    name = name.replaceAll(" ", "-").toLowerCase();
+    return parent ? `${parent.nodeKey}-${name}` : name;
+};
 
 export default {
-  name: "Links",
-  components: { ExpandLinks },
-  props: ["links"]
+    name: "Links",
+    props: ["links"],
+    methods: {
+        dataToNode(data) {
+            return DataTONode(data);
+        },
+    },
 };
 </script>
--- examples/home/src/data/Examples.js
+++ examples/home/src/data/Examples.js
@@ -1,12 +1,56 @@
 const Examples = {
+    BBCodeText: [
+        'BBCodetext',
+        'Scroll text'
+    ],
+    Board: {
+        'PathFinder': [
+            'Find path',
+        ],
+        'Match': [
+            'Line match'
+        ],
+        'FieldOfView': [
+            'FOV'
+        ],
+        'HexagonMap': [
+            'Hexagon'
+        ]
+    },
+    Canvas: [
+        'Fill'
+    ],
+    'Canvas data': [
+        'Text object to bitmap',
+        'Texture to color map'
+    ],
+    'Canvas texture': [
+        'circle'
+    ],
+    Chart: [
+        'Bar',
+        'Line'
+    ],
+    MaskedImage: [
+        'MaskedImage'
+    ],
+    TagText: [
+        'Tagtext'
+    ],
     UI: {
+        FixedWidthSizer: [
+            'Nested sizer'
+        ],
+        Gridsizer: [
+            'Grid'
+        ],
         Sizer: [
             'Nested sizer'
         ],
         Label: [
-            'label'
+            'Label'
         ],
-        'OverlapSizer': [
+        OverlapSizer: [
             'OverlapSizer'
         ]
     }
